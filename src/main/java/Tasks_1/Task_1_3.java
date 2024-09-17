eae37578b4465113b3fa6387ecfb25ef3554345f
Stanislaw1349 <alfstas@gmail.com>
2024-09-17 21:46:09 +0300

    refactoring classes

--- src/main/java/Tasks_1/Task_1_3.java
+++ src/main/java/Tasks_1/Task_1_3.java
@@ -1,2 +1,7 @@
-package Tasks_1;public class Task_1_3 {
+package Tasks_1;
+
+public class Task_1_3 {
+    public static void main(String[] args) {
+        System.out.println("\uD83D\uDE05");
+    }
 }
