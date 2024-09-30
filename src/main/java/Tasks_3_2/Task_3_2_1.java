f32cc136444357e1854258a83f83f6a90e2317f8
Stanislaw1349 <alfstas@gmail.com>
2024-09-30 22:47:00 +0300

    add Tasks 3.2.1, 3.2.2 3.2.3 for loop

---
+++ src/main/java/Tasks_3_2/Task_3_2_1.java
@@ -0,0 +1,12 @@
+package Tasks_3_2;
+
+public class Task_3_2_1 {
+    public static void main(String[] args) {
+
+        //Необходимо вывести числа от 0 до 15.
+
+        for (int i = 0; i < 16; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/Tasks_3_2/Task_3_2_2.java
@@ -0,0 +1,12 @@
+package Tasks_3_2;
+
+public class Task_3_2_2 {
+    public static void main(String[] args) {
+
+        //Необходимо вывести все положительные степени числа 5 которые меньше 10000, вывести результат возведения в степень.
+
+        for (int i = 5; i < 10000; i = i * 5) {
+            System.out.println(i);
+        }
+    }
+}
